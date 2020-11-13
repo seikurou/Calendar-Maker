3150bd92a5a7b763a586b6958511ec4dd25f042e
Edmond Fang <edmond.fang@gmail.com>
2020-11-12 20:43:30 -0800

    Rough main file

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 import argparse
+from docx import Document
+from docxcompose.composer import Composer
+from datetime import date, deltatime
+
+CALENDAR_TYPES = {'MonthlyLandscape': MonthlyLandscape}
+
+def makeCalendarCollection(args):
+    Calendar = CALENDAR_TYPES[args.type]
+    currDate = date(args.year, args.month, args.day)
+    calendarCollection = []
+    for i in range(args.cnt):
+        calendarCollection.append(Calendar(currDate))
+        currDate = Calendar.nextDate(currDate)
+
+def mergeDocuments(documents):
+    if not documents:
+        return
+    composer = Composer(documents[0])
+    for doc in range(1, len(documents)):
+        composer.append(doc)
+    return composer
 
 def main(args):
-    pass
+    calendarCollection = makeCalendarCollection(args)
+    composer = mergeDocuments(map(lambda x : x.makeDocument(), calendarCollection))
+    composer.save(args.filename + '.docx')
+
 if __name__ == "__main__":
+    assert CALENDAR_TYPES
     parser = argparse.ArgumentParser(description='Create printable calendars.')
-    parser.add_argument('--type', type=str, nargs=1, choices=['MonthlyLandscape'], default='MonthlyLandscape',
+    parser.add_argument('--type', type=str, nargs=1, choices=CALENDAR_TYPES.keys(), default=CALENDAR_TYPES.keys()[0],
                     help='Type of calendar',)
     parser.add_argument('--year', type=int, nargs=1, default=2020,
                                         help='Start year')
@@ -15,9 +40,11 @@ if __name__ == "__main__":
     parser.add_argument('--cnt', type=int, nargs=1, default=1,
                                         help='Number of calendars')
     parser.add_argument('--startday', type=str, nargs=1, default=6,
-                                        help='For monthly calendars, the day on which to start, default Sunday (6). Mon, Tues, Wed... is 0, 1, 2...',
+                                        help='For monthly/weekly calendars, the day on which to start, default Sunday (6). Mon, Tues, Wed... is 0, 1, 2...',
                                         choices=[0, 1, 2, 3, 4, 5, 6])
+    parser.add_argument('--filename', type=int, nargs=1, default='calendar',
+                                        help='Filename to save as, not including .docx')
 
 
     args = parser.parse_args()
-    main()
+    main(args)
